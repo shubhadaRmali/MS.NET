87ca13bb995147fbe620dd9f31915759565fd073
shubhadaRmali <rsm84164@gmail.com>
2024-06-27 17:16:22 +0530

    Add files via upload

---
+++ ArrayExamples/1DArray/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _1DArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] books = new string[5] { "Book-1", "Book-2", "Book-3", "Book-4", "Book-5"};
+            for (int i = 0; i < books.Length; i++)
+            {
+                Console.WriteLine(books[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ArrayExamples/JaggedArray/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaggedArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] marks = new int[3][];
+            marks[0] = new int[3] { 100, 200, 300 };
+            marks[1] = new int[5] { 100, 200, 300, 400, 500 };
+            marks[2] = new int[2] { 780, 980 };
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Row Number is {i}");
+                for (int j = 0; j < marks[i].Length; j++)
+                {
+                    Console.Write($"Col - {j} - {marks[i][j]}\t");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}

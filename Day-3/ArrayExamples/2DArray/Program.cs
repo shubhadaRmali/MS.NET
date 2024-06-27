fbdeef2f0a13bb57b0dbd07b6bebb6fa3dc4d6ab
shubhadaRmali <rsm84164@gmail.com>
2024-06-27 17:18:20 +0530

    Add files via upload

---
+++ Day-3/ArrayExamples/2DArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] data = new int[2, 2] { { 1, 100 }, { 2, 200 } };
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write($"{data[i,j]}\t");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
---
+++ Day-4/LinqExamples/LinqExamples/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace LinqExamples
+{
+    internal class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}

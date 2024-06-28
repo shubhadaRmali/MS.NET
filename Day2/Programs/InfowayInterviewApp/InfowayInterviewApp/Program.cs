52c1d3baf728864d3a4893f650e1d9b9f518189f
shubhadaRmali <rsm84164@gmail.com>
2024-06-28 16:55:17 +0530

    Add files via upload
    cdac infoway MS.NET

---
+++ Day2/Programs/InfowayInterviewApp/InfowayInterviewApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace InfowayInterviewApp
+{
+	internal class Program
+    {
+        static void Main(string[] args)
+        {
+			try
+			{
+				Student student=new Student();
+				student.OnSelection += new InterviewBasket(OracleDb);
+				student.OnSelection += new InterviewBasket(ExcelDb);
+				student.OnRejection+= new InterviewBasket(MsSqlServerDb);
+				student.StudentId = 1;
+				student.StudentName = "Test";
+				student.Email = "Test";
+				student.Phone= "37483434";
+				student.TotalMarks = 87;
+				if (student.TotalMarks>80 && student.TotalMarks<95)
+				{
+                    student.OnSelection -= new InterviewBasket(ExcelDb);
+                }
+				string company=string.Empty;
+				string result = student.CalculateResult(student.TotalMarks,ref company);
+				Console.WriteLine(result);
+				Console.WriteLine($"Interview for the Company - {company}!");
+				student.InterviewCities("Pune", "Bangalore", "Delhi", "Chennai");
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+			Console.ReadKey();
+        }
+
+		private static void OracleDb()
+		{
+			Console.WriteLine("Data is stored in Oracle DB!");
+		}
+        private static void ExcelDb()
+        {
+            Console.WriteLine("Data is stored in Excel DB!");
+        }
+        private static void MsSqlServerDb()
+        {
+            Console.WriteLine("Data is stored in Microsoft SQL Server DB!");
+        }
+    }
+}
---
+++ Day2/Programs/InfowayInterviewApp/InfowayInterviewApp/Student.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace InfowayInterviewApp
+{
+    public delegate void InterviewBasket();
+    internal class Student
+    {
+        //Actions/Events
+        public event InterviewBasket OnSelection;
+        public event InterviewBasket OnRejection;
+        public int StudentId { get; set; }
+        public string StudentName { get; set;}
+        public string Email { get; set;}
+        public string Phone { get; set;}
+        public int TotalMarks { get; set; }
+        //public string CalculateResult(int totalMarks,out string companyName)
+        //{
+        //    companyName = "Infosys Ltd.";
+        //    if (totalMarks>80)
+        //    {
+        //        return string.Format($"Student {StudentName} has been selected for next round of interview!");
+        //    }
+        //    else
+        //    {
+        //        return string.Format($"Student {StudentName} has been rejected for next round of interview!");
+        //    }
+        //}
+        public string CalculateResult(int totalMarks, ref string companyName)
+        {
+            companyName = "Infosys Ltd.";
+            if (totalMarks > 80)
+            {
+                if (OnSelection!=null)
+                {
+                    OnSelection();
+                }
+                return string.Format($"Student {StudentName} has been selected for next round of interview!");
+            }
+            else
+            {
+                if (OnRejection!=null)
+                {
+                    OnRejection();
+                }
+                return string.Format($"Student {StudentName} has been rejected for next round of interview!");
+            }
+        }
+        public void InterviewCities(params string[] cities)
+        {
+            foreach (string city in cities)
+            {
+                Console.WriteLine(city);
+            }
+        }
+    }
+}

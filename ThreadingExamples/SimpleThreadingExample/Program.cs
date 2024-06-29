7a717a925f05f6970ebcbd2491cb89cdc8af9ed3
shubhadaRmali <rsm84164@gmail.com>
2024-06-29 17:09:45 +0530

    Add files via upload

---
+++ ThreadingExamples/SimpleThreadingExample/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace SimpleThreadingExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Main Thread is running on Thread Id - {Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine("Main Thread Started!");
+            Thread t1 = new Thread(new ThreadStart(Job1));
+            Thread t2 = new Thread(new ThreadStart(Job2));
+            t1.Priority=ThreadPriority.Lowest; 
+            t2.Priority=ThreadPriority.Highest;
+            t2.Start();
+            t1.Start();
+            t1.Join();
+            t2.Join();
+            
+            Console.WriteLine("Main Thread Ended!");
+        }
+        private static void Job1()
+        {
+            Console.WriteLine($"Job-1 Thread is running on Thread Id - {Thread.CurrentThread.ManagedThreadId}");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Job - 1 and Step - {i}");
+            }
+        }
+        private static void Job2()
+        {
+            Console.WriteLine($"Job-2 Thread is running on Thread Id - {Thread.CurrentThread.ManagedThreadId}");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Job - 2 and Step - {i}");
+            }
+        }
+    }
+}
---
+++ ThreadingExamples/ThreadSynchronization/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadSynchronization
+{
+    internal class Program
+    {
+        static object o=new object();
+        static int TicketNo = 1;
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Main Thread is running on Thread Id - {Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine("Main Thread Started!");
+            Thread t1 = new Thread(new ThreadStart(TicketWindow1));
+            Thread t2 = new Thread(new ThreadStart(TicketWindow2));
+            //t1.Priority = ThreadPriority.Lowest;
+            //t2.Priority = ThreadPriority.Highest;
+            t1.Start();
+            t2.Start();
+            t1.Join();
+            t2.Join();
+
+            Console.WriteLine("Main Thread Ended!");
+        }
+        private static void TicketWindow1()
+        {
+            Monitor.Enter(o);
+            //lock (o)
+            //{
+                Console.WriteLine($"Window-1 Thread is running on Thread Id - {Thread.CurrentThread.ManagedThreadId}");
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine($"Window - 1 and Step - {TicketNo}");
+                    TicketNo++;
+                    Thread.Sleep(1000);
+                }
+            //}
+            Monitor.Exit(o);
+            
+        }
+        private static void TicketWindow2()
+        {
+            
+            lock (o)
+            {
+                Console.WriteLine($"Window-2 Thread is running on Thread Id - {Thread.CurrentThread.ManagedThreadId}");
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine($"Window - 2 and Step - {TicketNo}");
+                    TicketNo++;
+                } 
+            }
+        }
+    }
+}
